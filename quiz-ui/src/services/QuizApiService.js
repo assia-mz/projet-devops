87512a67eaf8a86c7e74f253e2a273a39debf930
Aissatou <cissaissatou25@gmail.com>
2025-10-20 18:28:19 +0200

    3.3

--- quiz-ui/src/services/QuizApiService.js
+++ quiz-ui/src/services/QuizApiService.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
-  json: true,
+	baseURL: `${import.meta.env.VITE_API_URL}`,
+  json: true
 });
 
 export default {
   async call(method, resource, data = null, token = null) {
     var headers = {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
     if (token != null) {
-      headers.authorization = 'Bearer ' + token;
+      headers.authorization = "Bearer " + token;
     }
 
     return instance({
@@ -28,9 +28,10 @@ export default {
       });
   },
   getQuizInfo() {
-    return this.call('get', 'quiz-info');
+    return this.call("get", "quiz-info");
   },
   getQuestion(position) {
     // not implemented
-  },
+    return position; //à enlever
+  }
 };
---
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="new-quiz">
+    <p1>Saisissez votre nom</p1>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        </div>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        <button type="button" class="btn btn-primary">GO!</button>
+    </div>
+
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
